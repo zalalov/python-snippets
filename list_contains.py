02ab6c37a883d183f1860bd3ceb98da86d8e351f
Ruslan Zalalov <zalalov@mail.ru>
2016-10-09 18:44:21 +0300

    Boundary values analysis

--- list_contains.py
+++ list_contains.py
@@ -1,7 +1,10 @@
 def contains(needed, seq):
     """ Check if list contains needed """
+    if not isinstance(needed, (list, tuple)) or not isinstance(seq, (list, tuple)):
+        raise Exception("Arguments should be lists of tuples")
+
     index = [i for i in range(len(seq)) if seq[i:i + len(needed)] == needed]
     return bool(index)
 
 print contains([4], [1,2,3,4,5])
-
+print contains(None, None)
